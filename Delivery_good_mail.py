f0f413637947305f02bea1821d5aabb9005dc39d
deepshrivastava2493-beep <deepshrivastava2493@gmail.com>
2025-08-24 14:25:05 +0530

    Update Delivery_good_mail.py

--- Delivery_good_mail.py
+++ Delivery_good_mail.py
@@ -9,6 +9,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import json
 from datetime import datetime
+import os
 
 # ---- CONFIG ----
 DELIVERY_THRESHOLD = 85
